fadeb2d411519bf5843028f05b3a1929bef555e9
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-09-16 15:50:32 -0700

    updated next config with cache control

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     domains: [process.env.IMAGE_URL],
   },
+  async headers() {
+    return [
+      {
+        source: "/projects",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
