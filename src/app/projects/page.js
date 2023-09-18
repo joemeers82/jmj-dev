86c02f4f2dc12ff09d3646dd86e3acc9fd73053d
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-09-18 10:12:24 -0700

    fixed key  miss

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,7 +9,10 @@ export default async function Page({ params }) {
       {projectsPage.map((project, i) => {
         const data = project.project_data.data;
         return (
-          <article className="w-full max-w-7xl mx-auto p-6 bg-gray-100 shadow-lg rounded-md my-10">
+          <article
+            key={i}
+            className="w-full max-w-7xl mx-auto p-6 bg-gray-100 shadow-lg rounded-md my-10"
+          >
             <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
               <section className="w-full md:w-1/2 p-3">
                 <Image
@@ -60,7 +63,10 @@ export default async function Page({ params }) {
                 <div className="mt-6 flex justify-center md:justify-start gap-3">
                   {data.project_tools.map((tool, i) => {
                     return (
-                      <div className="flex items-center justify-center cursor-pointer p-2 h-[60px] w-[60px] bg-white rounded-full shadow bg-no-repeat">
+                      <div
+                        key={i}
+                        className="flex items-center justify-center cursor-pointer p-2 h-[60px] w-[60px] bg-white rounded-full shadow bg-no-repeat"
+                      >
                         <Image
                           title={tool}
                           alt={`${tool} Icon`}
