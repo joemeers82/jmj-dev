f3eeb213ed7a1c3a3226979c48ec9441b510b2e4
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-09-18 10:10:07 -0700

    fixed class/className miss

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -8,20 +8,19 @@ export default async function Page({ params }) {
     <main className="flex min-h-screen flex-col items-center justify-between p-12 md:p-20">
       {projectsPage.map((project, i) => {
         const data = project.project_data.data;
-        console.log(data.github_url);
         return (
-          <article class="w-full max-w-7xl mx-auto p-6 bg-gray-100 shadow-lg rounded-md my-10">
-            <div class="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
-              <section class="w-full md:w-1/2 p-3">
+          <article className="w-full max-w-7xl mx-auto p-6 bg-gray-100 shadow-lg rounded-md my-10">
+            <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
+              <section className="w-full md:w-1/2 p-3">
                 <Image
                   src={project.project_image}
                   width={500}
                   height={500}
                   alt={data.project_title}
                 />
-                <nav class="flex items-center gap-5 pl-5">
+                <nav className="flex items-center gap-5 pl-5">
                   {data.has_github_url === "1" && (
-                    <div class="mt-3 flex items-center space-x-2">
+                    <div className="mt-3 flex items-center space-x-2">
                       <Link
                         href={data.github_url.url}
                         target={data.github_url.target}
@@ -36,7 +35,7 @@ export default async function Page({ params }) {
                       </Link>
                     </div>
                   )}
-                  <div class="mt-3">
+                  <div className="mt-3">
                     <Link
                       href={data.project_url.url}
                       target={data.project_url.target}
@@ -53,15 +52,15 @@ export default async function Page({ params }) {
                 </nav>
               </section>
 
-              <section class="w-full md:w-1/2 p-3">
-                <h1 class="py-5 text-4xl text-left border-b mb-5">
+              <section className="w-full md:w-1/2 p-3">
+                <h1 className="py-5 text-4xl text-left border-b mb-5">
                   {data.project_title}
                 </h1>
                 <p dangerouslySetInnerHTML={{ __html: data.project_about }} />
-                <div class="mt-6 flex justify-center md:justify-start gap-3">
+                <div className="mt-6 flex justify-center md:justify-start gap-3">
                   {data.project_tools.map((tool, i) => {
                     return (
-                      <div class="flex items-center justify-center cursor-pointer p-2 h-[60px] w-[60px] bg-white rounded-full shadow bg-no-repeat">
+                      <div className="flex items-center justify-center cursor-pointer p-2 h-[60px] w-[60px] bg-white rounded-full shadow bg-no-repeat">
                         <Image
                           title={tool}
                           alt={`${tool} Icon`}
