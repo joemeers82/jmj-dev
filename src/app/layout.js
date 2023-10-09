e0e7816d40766426dacc4af7f16ef3b002405e4e
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-10-09 16:28:44 -0700

    added gtm tag fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,7 +42,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           dangerouslySetInnerHTML={{
             __html: `
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7RP95G6"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
 
             `,
           }}
