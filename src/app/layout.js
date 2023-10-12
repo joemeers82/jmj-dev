cf5a2dabb30f825d5dbd6d82639f5fe04208efc5
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-10-12 11:53:35 -0700

    fixed crossorigin attribute in layoutjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         <script
           src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"
           integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q=="
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
           referrerpolicy="no-referrer"
           async
         ></script>
