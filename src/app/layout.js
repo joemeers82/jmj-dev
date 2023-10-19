260b99aa4efba337644d4618582243d66612dc62
Joe Meers Jankowski <joemeers82@gmail.com>
2023-10-19 11:14:23 -0700

    fixed referrerPolicy typo  in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
           src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"
           integrity="sha512-z4OUqw38qNLpn1libAN9BsoDx6nbNFio5lA6CuTp9NlK83b89hgyCVq+N5FdBJptINztxn1Z3SaKSKUS5UP60Q=="
           crossOrigin="anonymous"
-          referrerpolicy="no-referrer"
+          referrerPolicy="no-referrer"
           async
         ></script>
 
